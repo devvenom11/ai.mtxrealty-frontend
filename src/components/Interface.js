8887a7d82feb26ec47766e6db4e44ae7022d1899
jkr86 <johnyrj97@gmail.com>
2024-09-24 01:12:15 +0500

    include  link

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -1,33 +1,31 @@
 import { PlusIcon, SubmitArrow } from "@/icons/Icons";
 import { useState, useEffect } from "react";
 
-
 const ChatInterface = ({ chat, setChats }) => {
   const [input, setInput] = useState("");
   const [height, setHeight] = useState(0);
 
-
-
   useEffect(() => {
     const updateHeight = () => {
-      setHeight(window.innerHeight-70);
+      setHeight(window.innerHeight - 70);
     };
     updateHeight();
-    window.addEventListener('resize', updateHeight);
-    return () => window.removeEventListener('resize', updateHeight);
+    window.addEventListener("resize", updateHeight);
+    return () => window.removeEventListener("resize", updateHeight);
   }, []);
-
-
+  const handleResponse = (data) => {
+    let res = data.answer || "I don't have an answer for that.";
+    const links = data.youtube_url ? `<a target='_blank' href="${data.youtube_url}" style="color: blue;">${data.youtube_url}</a><br>` : "";
+    
+    return `${links} ${res}`.trim();
+  };
   const handleSendMessage = async (e) => {
     e.preventDefault();
     if (input.trim()) {
       const userMessage = input;
       setInput("");
 
-      const updatedMessages = [
-        ...chat.messages,
-        { text: userMessage, sender: "user" },
-      ];
+      const updatedMessages = [...chat.messages, { text: userMessage, sender: "user" }];
       updateChatMessages(updatedMessages, true, "", chat.id);
 
       try {
@@ -44,30 +42,15 @@ const ChatInterface = ({ chat, setChats }) => {
 
         if (response.ok) {
           const data = await response.json();
-          const botResponse = data.answer || "I don't have an answer for that.";
-          simulateBotTyping(
-            botResponse,
-            updatedMessages,
-            data.session_id,
-            chat.id
-          );
+          const botResponse = handleResponse(data);
+          simulateBotTyping(botResponse, updatedMessages, data.session_id, chat.id);
         } else {
           console.error("API call failed with status:", response.status);
-          simulateBotTyping(
-            "Something went wrong. Please try again later.",
-            updatedMessages,
-            chat.sessionId,
-            chat.id
-          );
+          simulateBotTyping("Something went wrong. Please try again later.", updatedMessages, chat.sessionId, chat.id);
         }
       } catch (error) {
         console.error("Error:", error);
-        simulateBotTyping(
-          "Error connecting to the server. Please try again.",
-          updatedMessages,
-          chat.sessionId,
-          chat.id
-        );
+        simulateBotTyping("Error connecting to the server. Please try again.", updatedMessages, chat.sessionId, chat.id);
       }
     }
   };
@@ -76,22 +59,23 @@ const ChatInterface = ({ chat, setChats }) => {
     let currentMessage = "";
     let index = 0;
     const interval = setInterval(() => {
-      if (index < text.length) {
+      if (index < text.length && !text.startsWith("<")) {
+        // Regular typing for plain text
         currentMessage += text.charAt(index);
-        updateChatMessages(chats, true, currentMessage, chatId, sessionId); 
+        updateChatMessages(chats, true, currentMessage, chatId, sessionId);
         index++;
       } else {
+        // When we encounter HTML, clear interval and update message directly
         clearInterval(interval);
         const updatedMessages = [...chats, { text, sender: "bot" }];
-        updateChatMessages(updatedMessages, false, "", chatId, sessionId); 
+        updateChatMessages(updatedMessages, false, "", chatId, sessionId);
       }
     }, 50);
   };
 
   const getTitle = (text) => {
     function containsGreeting(text) {
-      const greetings =
-        /\b(hello|hi|hey|greetings|good\s*morning|good\s*afternoon|good\s*evening)\b/i;
+      const greetings = /\b(hello|hi|hey|greetings|good\s*morning|good\s*afternoon|good\s*evening)\b/i;
       return greetings.test(text);
     }
     if (containsGreeting(text)) {
@@ -100,14 +84,9 @@ const ChatInterface = ({ chat, setChats }) => {
       return text.length > 26 ? text.slice(0, 26) + "..." : text;
     }
   };
+  
 
-  const updateChatMessages = (
-    updatedMessages,
-    isTyping,
-    botMessage,
-    chatId,
-    sessionId
-  ) => {
+  const updateChatMessages = (updatedMessages, isTyping, botMessage, chatId, sessionId) => {
     setChats((prevChats) =>
       prevChats.map((c) =>
         c.id === chatId
@@ -143,37 +122,22 @@ const ChatInterface = ({ chat, setChats }) => {
           message.sender === "user" ? (
             <div key={index} className="mb-4 flex">
               <div className="bg-background flex size-[25px] shrink-0 select-none items-center justify-center rounded-md border shadow-sm">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 256 256"
-                  fill="currentColor"
-                  className="size-4"
-                >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor" className="size-4">
                   <path d="M230.92 212c-15.23-26.33-38.7-45.21-66.09-54.16a72 72 0 1 0-73.66 0c-27.39 8.94-50.86 27.82-66.09 54.16a8 8 0 1 0 13.85 8c18.84-32.56 52.14-52 89.07-52s70.23 19.44 89.07 52a8 8 0 1 0 13.85-8ZM72 96a56 56 0 1 1 56 56 56.06 56.06 0 0 1-56-56Z" />
                 </svg>
               </div>
-              <div className="ml-4 flex-1 space-y-2 overflow-hidden pl-1 title">
-                {message.text}
-              </div>
+              <div className="ml-4 flex-1 space-y-2 overflow-hidden pl-1 title">{message.text}</div>
             </div>
           ) : (
             <div key={index} className="mb-4 flex">
               <div className="bg-primary text-primary-foreground flex size-[24px] shrink-0 select-none items-center justify-center rounded-md border shadow-sm">
-                <svg
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-4"
-                >
+                <svg fill="currentColor" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" className="size-4">
                   <title>Bot icon</title>
                   <path d="M22.2819 9.8211a5.9847 5.9847 0 0 0-.5157-4.9108 6.0462 6.0462 0 0 0-6.5098-2.9A6.0651 6.0651 0 0 0 4.9807 4.1818a5.9847 5.9847 0 0 0-3.9977 2.9 6.0462 6.0462 0 0 0 .7427 7.0966 5.98 5.98 0 0 0 .511 4.9107 6.051 6.051 0 0 0 6.5146 2.9001A5.9847 5.9847 0 0 0 13.2599 24a6.0557 6.0557 0 0 0 5.7718-4.2058 5.9894 5.9894 0 0 0 3.9977-2.9001 6.0557 6.0557 0 0 0-.7475-7.0729zm-9.022 12.6081a4.4755 4.4755 0 0 1-2.8764-1.0408l.1419-.0804 4.7783-2.7582a.7948.7948 0 0 0 .3927-.6813v-6.7369l2.02 1.1686a.071.071 0 0 1 .038.052v5.5826a4.504 4.504 0 0 1-4.4945 4.4944zm-9.6607-4.1254a4.4708 4.4708 0 0 1-.5346-3.0137l.142.0852 4.783 2.7582a.7712.7712 0 0 0 .7806 0l5.8428-3.3685v2.3324a.0804.0804 0 0 1-.0332.0615L9.74 19.9502a4.4992 4.4992 0 0 1-6.1408-1.6464zM2.3408 7.8956a4.485 4.485 0 0 1 2.3655-1.9728V11.6a.7664.7664 0 0 0 .3879.6765l5.8144 3.3543-2.0201 1.1685a.0757.0757 0 0 1-.071 0l-4.8303-2.7865A4.504 4.504 0 0 1 2.3408 7.872zm16.5963 3.8558L13.1038 8.364 15.1192 7.2a.0757.0757 0 0 1 .071 0l4.8303 2.7913a4.4944 4.4944 0 0 1-.6765 8.1042v-5.6772a.79.79 0 0 0-.407-.667zm2.0107-3.0231l-.142-.0852-4.7735-2.7818a.7759.7759 0 0 0-.7854 0L9.409 9.2297V6.8974a.0662.0662 0 0 1 .0284-.0615l4.8303-2.7866a4.4992 4.4992 0 0 1 6.6802 4.66zM8.3065 12.863l-2.02-1.1638a.0804.0804 0 0 1-.038-.0567V6.0742a4.4992 4.4992 0 0 1 7.3757-3.4537l-.142.0805L8.704 5.459a.7948.7948 0 0 0-.3927.6813zm1.0976-2.3654l2.602-1.4998 2.6069 1.4998v2.9994l-2.5974 1.4997-2.6067-1.4997Z" />
                 </svg>
               </div>
               <div className="ml-4 flex-1 space-y-2 overflow-hidden px-1">
-                <p className="prose prose-p:leading-relaxed prose-pre:p-0 break-words">
-                  {message.text}
-                </p>
+                <p dangerouslySetInnerHTML={{__html:message.text}} className="prose prose-p:leading-relaxed prose-pre:p-0 break-words"></p>
               </div>
             </div>
           )
@@ -181,13 +145,7 @@ const ChatInterface = ({ chat, setChats }) => {
         {chat.isTyping && (
           <div className="mb-4 flex justify-start">
             <div className="bg-primary text-primary-foreground flex size-[24px] shrink-0 select-none items-center justify-center rounded-md border shadow-sm">
-              <svg
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4"
-              >
+              <svg fill="currentColor" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" className="size-4">
                 <title>Bot typing</title>
                 <path d="M22.2819 9.8211a5.9847 5.9847 0 0 0-.5157-4.9108 6.0462 6.0462 0 0 0-6.5098-2.9A6.0651 6.0651 0 0 0 4.9807 4.1818a5.9847 5.9847 0 0 0-3.9977 2.9 6.0462 6.0462 0 0 0 .7427 7.0966 5.98 5.98 0 0 0 .511 4.9107 6.051 6.051 0 0 0 6.5146 2.9001A5.9847 5.9847 0 0 0 13.2599 24a6.0557 6.0557 0 0 0 5.7718-4.2058 5.9894 5.9894 0 0 0 3.9977-2.9001 6.0557 6.0557 0 0 0-.7475-7.0729zm-9.022 12.6081a4.4755 4.4755 0 0 1-2.8764-1.0408l.1419-.0804 4.7783-2.7582a.7948.7948 0 0 0 .3927-.6813v-6.7369l2.02 1.1686a.071.071 0 0 1 .038.052v5.5826a4.504 4.504 0 0 1-4.4945 4.4944zm-9.6607-4.1254a4.4708 4.4708 0 0 1-.5346-3.0137l.142.0852 4.783 2.7582a.7712.7712 0 0 0 .7806 0l5.8428-3.3685v2.3324a.0804.0804 0 0 1-.0332.0615L9.74 19.9502a4.4992 4.4992 0 0 1-6.1408-1.6464zM2.3408 7.8956a4.485 4.485 0 0 1 2.3655-1.9728V11.6a.7664.7664 0 0 0 .3879.6765l5.8144 3.3543-2.0201 1.1685a.0757.0757 0 0 1-.071 0l-4.8303-2.7865A4.504 4.504 0 0 1 2.3408 7.872zm16.5963 3.8558L13.1038 8.364 15.1192 7.2a.0757.0757 0 0 1 .071 0l4.8303 2.7913a4.4944 4.4944 0 0 1-.6765 8.1042v-5.6772a.79.79 0 0 0-.407-.667zm2.0107-3.0231l-.142-.0852-4.7735-2.7818a.7759.7759 0 0 0-.7854 0L9.409 9.2297V6.8974a.0662.0662 0 0 1 .0284-.0615l4.8303-2.7866a4.4992 4.4992 0 0 1 6.6802 4.66zM8.3065 12.863l-2.02-1.1638a.0804.0804 0 0 1-.038-.0567V6.0742a4.4992 4.4992 0 0 1 7.3757-3.4537l-.142.0805L8.704 5.459a.7948.7948 0 0 0-.3927.6813zm1.0976-2.3654l2.602-1.4998 2.6069 1.4998v2.9994l-2.5974 1.4997-2.6067-1.4997Z" />
               </svg>
@@ -208,7 +166,7 @@ const ChatInterface = ({ chat, setChats }) => {
               className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input shadow-sm hover:bg-accent hover:text-accent-foreground h-9 w-9 bg-background absolute left-0 top-[13px] size-8 rounded-full p-0 sm:left-4"
               data-state="closed"
             >
-             <PlusIcon />
+              <PlusIcon />
               <span className="sr-only">New Chat</span>
             </button>
             <textarea
@@ -230,7 +188,7 @@ const ChatInterface = ({ chat, setChats }) => {
                 type="submit"
                 disabled={input.length < 2}
               >
-               <SubmitArrow />
+                <SubmitArrow />
                 <span className="sr-only">Send message</span>
               </button>
             </div>
